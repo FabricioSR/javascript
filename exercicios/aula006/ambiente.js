f12d7a07e2f38c3a9043d0de671f26ad8877d0a7
FabricioSR <fabricio-rreis@hotmail.com>
2022-01-10 10:13:42 -0300

    mais JS

--- exercicios/aula006/ambiente.js
+++ exercicios/aula006/ambiente.js
@@ -1,6 +1,7 @@
-console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')
-console.log('tudo bem')
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6 )
+
+
